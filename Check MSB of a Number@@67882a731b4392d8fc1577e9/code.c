f41e2d2cd65d916109069abf9946a0f2ae7315af
SudhanshMehta01 <sudhanshmehtavlogs@gmail.com>
2025-02-07 18:32:15 +0530

    Solution to Check MSB of a Number

---
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input a number from user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check if the LSB (Least Significant Bit) is set or not
+    if (num & 1)
+        printf("Set\n");  // LSB is 1
+    else
+        printf("Not Set\n");  // LSB is 0
+
+    return 0;
+}
